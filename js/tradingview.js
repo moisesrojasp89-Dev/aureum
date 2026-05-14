febf8e85a5bf51c9cf3ae31a031741d670f910e9
moisesrojasp89-Dev <moisesrojasp.89@gmail.com>
2026-05-14 01:35:17 -0400

    Update tradingview.js

--- js/tradingview.js
+++ js/tradingview.js
@@ -1,14 +1,1 @@
-const chartContainer = document.getElementById("tradingview-chart");
 
-if (chartContainer) {
-    chartContainer.innerHTML = `
-        <iframe
-            src="https://s.tradingview.com/widgetembed/?symbol=BINANCE:BTCUSDT&interval=240&theme=dark&style=1&locale=es"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            allowtransparency="true"
-            scrolling="no">
-        </iframe>
-    `;
-}
